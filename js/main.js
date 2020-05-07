6f0d90205fbd4e5123fe57b2e0e2e0c5d958d8d3
Aleksandr Piatopal <piatopal.alex@gmail.com>
2020-05-07 19:26:33 +0300

    r

--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
 $(function () {
+    var header = $("header");
+    var introH = $("#intro").innerHeight();
+    var scrollOffset = $(window).scrollTop();
+
+       // Fixed header for mobile menu
+       checkScroll(scrollOffset);
+       $(window).on("scroll", function () {
+           scrollOffset = $(this).scrollTop();
+           checkScroll(scrollOffset);
+           console.log(introH);
+       });
+   
+       function checkScroll(scrollOffset) {
+           if (scrollOffset >= introH) {
+               header.addClass("fixed");
+           } else {
+               header.removeClass("fixed");
+           }
+       }
+       
+           // Smooth scroll
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        var $this = $(this);
+        var blockId = $this.data("scroll");
+        var blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $("#nav").removeClass("open");
+
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+
 
     // Mobile menu
     $("#menu_btn").on("click", function (event) {
         event.preventDefault();
         $("#nav").toggleClass("open");
+       
     });
 
 });
