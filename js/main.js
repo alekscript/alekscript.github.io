cf1ad0a1e514acb9ad415ce446c0189a7036c9b9
Aleksandr Piatopal <piatopal.alex@gmail.com>
2020-05-09 08:09:01 +0300

    R

--- js/main.js
+++ js/main.js
@@ -45,12 +45,6 @@ $(function () {
        
     });
 
-    // Проверка на ввод цифр
-    $(".phone").keypress(function(event){
-        event = event || window.event;
-        if (event.charCode && event.charCode!=0 && event.charCode!=46 && (event.charCode < 48 || event.charCode > 57) )
-          return false;
-      });
 
   /* Modal
     ================= */
